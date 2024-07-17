0d61c5738864807c64d0e68fc0b51b2a0e912922
EngineeringCode <engineeringcode93@gmail.com>
2024-07-17 22:49:43 +0900

    1. 파이썬 권장 위치로 import 위치 변경 2. input 명칭 명확하 3. 테스트 케이스를 주석에 추가

--- softeer/6254.py
+++ softeer/6254.py
@@ -1,14 +1,11 @@
-"""
+import sys
 
+"""
 연습문제 > 근무시간
 문제 주소: https://softeer.ai/practice/6254
 해설 주소:
-
 프로그램 작성자: 공학코드(engineeringcode93@gmail.com)
-
 """
-
-import sys
 
 work_sum = 0
 
@@ -16,10 +13,10 @@ for i in range(5):
 
     one_line = sys.stdin.readline()
 
-    lh = int(input[0:2])
-    lm = int(input[3:5])
-    rh = int(input[6:8])
-    rm = int(input[9:11])
+    lh = int(one_line[0:2])
+    lm = int(one_line[3:5])
+    rh = int(one_line[6:8])
+    rm = int(one_line[9:11])
 
     if rm - lm < 0:
         rh = rh - 1
@@ -30,3 +27,35 @@ for i in range(5):
     work_sum = work_sum + rm
 
 print(work_sum)
+
+"""
+Case 1
+Input
+10:00 19:00
+09:00 15:00
+10:00 11:00
+11:00 22:00
+09:00 15:00
+Output
+1980
+
+Case 2
+Input
+10:00 19:00
+09:00 15:00
+10:00 11:00
+11:00 22:00
+09:00 15:00
+Output
+2785
+
+Case 3
+Input
+09:17 19:24
+10:11 18:45
+09:34 18:27
+10:47 15:33
+08:47 18:32
+Output
+2525
+"""
